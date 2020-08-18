5d4e4abab037d78ef11e110adf114586f323aaae
teguhbs7 <69722686+teguhbs7@users.noreply.github.com>
2020-08-18 21:01:04 +0800

    Update tbs-menu.js

--- tbs-menu.js
+++ tbs-menu.js
@@ -1,15 +1,53 @@
-// Navbar
+// Aside Navbar
     const nav=document.querySelector(".nav-tbs"),
           navList=nav.querySelectorAll("li"),
-          totalNavList=navList.length;
+          totalNavList=navList.length,
+          allSection=document.querySelectorAll(".section"),
+          totalSection=allSection.length;
 
     for(let i=0; i<totalNavList; i++){
-        const li=navList[i].querySelector("li");
-        li.addEventListener("click", function(){
+        const a=navList[i].querySelector("a");
+        a.addEventListener("click", function(){
+            // remove back section class
+            removeBackSectionClass();
+
             for(let j=0; j<totalNavList; j++){
-                navList[j].querySelector("li").classList.remove("active");
+                if(navList[j].querySelector("a").classList.contains("active")){
+                    // add back section class
+                    addBackSectionClass(j)
+                }
+                navList[j].querySelector("a").classList.remove("active");
             }
-            
-        this.classList.add("active");
+            this.classList.add("active");
+            showSection(this);
+
+            if(window.innerWidth < 1200){
+                asideSectionTogglerBtn();
+            }
         })
+    }
+    function addBackSectionClass(num){
+        allSection[num].classList.add("back-section");
+    }
+    function removeBackSectionClass(){
+        for(let i=0; i<totalSection; i++){
+            allSection[i].classList.remove("back-section");
+        }
+    }
+    function showSection(element){
+            for(let i=0; i<totalSection; i++){
+                allSection[i].classList.remove("active");
+            }
+        const target=element.getAttribute("href").split("#")[1];
+      document.querySelector("#"+target).classList.add("active")
+    }
+
+    function updateNav(element){
+        for(let i=0; i<totalNavList; i++){
+            navList[i].querySelector("a").classList.remove("active");
+            const target=element.getAttribute("href").split("#")[1];
+            if(target === navList[i].querySelector("a").getAttribute("href").split("#")[1]){
+                navList[i].querySelector("a").classList.add("active");
+            }
+        }
     }
