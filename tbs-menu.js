8d3371b372bf3da5ae0e7b555edc2a4c6ad9b521
teguhbs7 <69722686+teguhbs7@users.noreply.github.com>
2020-08-18 17:07:41 +0800

    Update tbs-menu.js

--- tbs-menu.js
+++ tbs-menu.js
@@ -1,23 +1,8 @@
 // Aside Navbar
     const nav=document.querySelector(".nav-tbs"),
           navList=nav.querySelectorAll("li"),
-          totalNavList=navList.length,
-          allSection=document.querySelectorAll(".section"),
-          totalSection=allSection.length;
-          
-for(let i=0; i<totalNavList; i++){
-        const a=navList[i].querySelector("a");
-        a.addEventListener("click", function(){
-            // remove back section class
-            removeBackSectionClass();
+          totalNavList=navList.length;
 
-            for(let j=0; j<totalNavList; j++){
-                if(navList[j].querySelector("a").classList.contains("active")){
-                    // add back section class
-                    addBackSectionClass(j)
-                }
-                navList[j].querySelector("a").classList.remove("active");
-            }
-            this.classList.add("active");
-            showSection(this);
+    for(let i=0; i<totalNavList; i++){
+        console.log(navList[i])
     }
