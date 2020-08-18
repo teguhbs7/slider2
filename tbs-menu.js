40ef59a3ba373137857802cb648334f40408a31d
teguhbs7 <69722686+teguhbs7@users.noreply.github.com>
2020-08-18 17:46:16 +0800

    Update tbs-menu.js

--- tbs-menu.js
+++ tbs-menu.js
@@ -6,6 +6,10 @@
     for(let i=0; i<totalNavList; i++){
         const a=navList[i].querySelector("a");
         a.addEventListener("click", function(){
-        this.classList.add("active")
+            for(let j=0; j<totalNavList; j++){
+                navList[j].querySelector("a").classList.remove("active");
+            }
+            
+        this.classList.add("active");
         })
     }
