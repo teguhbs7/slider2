9dc51fb5cea1f5e0be95d932637429309bc269e5
teguhbs7 <69722686+teguhbs7@users.noreply.github.com>
2020-08-18 21:09:35 +0800

    Update tbs-webengine.js

--- tbs-webengine.js
+++ tbs-webengine.js
@@ -1,1 +1,15 @@
-a
+// Navbar
+    const nav=document.querySelector('.nav-tbs'),
+          navList=nav.querySelectorAll('li'),
+          totalNavList=navList.length;
+
+    for(let i=0; i<totalNavList; i++){
+        const li=navList[i].querySelector('li');
+        li.addEventListener('click', function(){
+            for(let j=0; j<totalNavList; j++){
+                navList[j].querySelector("li").classList.remove('active');
+            }
+            
+        this.classList.add('active');
+        })
+    }
