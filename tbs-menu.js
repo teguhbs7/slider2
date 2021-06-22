beb4a427da7cec6e5bcd11bcbd2764d545fe8671
teguhbs7 <69722686+teguhbs7@users.noreply.github.com>
2021-06-22 14:13:16 +0800

    Update tbs-menu.js

--- tbs-menu.js
+++ tbs-menu.js
@@ -1,23 +1,3 @@
-// Back To Top
-var mybutton = document.getElementById("myBtn");
-
-// When the user scrolls down 20px from the top of the document, show the button
-window.onscroll = function() {scrollFunction()};
-
-function scrollFunction() {
-  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-    mybutton.style.display = "block";
-  } else {
-    mybutton.style.display = "none";
-  }
-}
-
-// When the user clicks on the button, scroll to the top of the document
-function topFunction() {
-  document.body.scrollTop = 0;
-  document.documentElement.scrollTop = 0;
-
-
 // Navbar
 const nav=document.querySelector(".nav-tbs"),
       navList=nav.querySelectorAll("li"),
@@ -61,7 +41,27 @@ function removeBackSectionClass(){
   }
 }
 
+// Back To Top
+var mybutton = document.getElementById("myBtn");
 
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+  
+  
+  
 
 // Pull to Refresh
 /*!
